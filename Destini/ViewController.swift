a0d3df530345d1affd28f110e8d0a19e47dbf168
AhmedAmr77 <ahmed.sharf.777@gmail.com>
2020-06-12 07:44:40 -0700

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Ahmd Amr on 6/12/20.
+//  Copyright © 2020 Ahmd Amr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
